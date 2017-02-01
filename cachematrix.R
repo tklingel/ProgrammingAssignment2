9ea5c4c8110ee85c2463be177247a8ef443ec910
Thomas Klingel <thomas.klingel@t-systems.com>
2017-02-01 16:54:49 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## I edited this file in github, because I used my Company Notebook, and it does 
+## not allow me to clone theis to git because of the firewall-restrictions
+## while I have not other Notebook than the Companies this was the only way to try to pass thiss assignment
+## Sorry for that - hope it works - regards Thomas
+##
+##
 ## Put comments here that give an overall description of what your
 ## functions do
 
